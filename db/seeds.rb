48e14408ba9050207e5cf1c4a6c31751ea44e28a
Clarisse Kessab <clarisseksb@gmail.com>
2024-11-13 12:17:39 +0100

    DB Create + Validation opérationnel

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do |i|
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_name, phone_number:Faker::PhoneNumber.phone_number, category: Restaurant::CATEGORIES.sample)
+end
